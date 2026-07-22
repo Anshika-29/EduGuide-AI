37e43269934adf1f13c12e471ab6556bff5745af
Anshika Singh <144808439+Anshika-29@users.noreply.github.com>
2026-07-22 23:16:45 +0530

    Setup backend server and configure environment

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const lectureRoutes=require("./routes/lectureRoutes");
+const express=require("express");
+const cors=require("cors");
+require("dotenv").config();
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/lectures",lectureRoutes);
+const PORT=process.env.PORT||5000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
